f3caa35a5048d0fee2d43de3826175b6e731d6a8
okanDerecik <96899474+okanDerecik@users.noreply.github.com>
2022-09-04 15:19:07 +0300

    Add project files.

---
+++ Test_Trigger/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Test_Trigger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-K0C08G8;Initial Catalog=Test;Integrated Security=True");
+
+        void listele()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * From TBLKITAPLAR", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        void sayac()
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select * From TblSAYAC",baglanti);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                LblKitap.Text = dr[0].ToString();
+            }
+            baglanti.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+            sayac();
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Insert into TBLKITAPLAR (AD,YAZAR,SAYFA,YAYINEVI,TUR) VALUES (@p1,@p2,@p3,@p4,@p5)", baglanti);
+            komut.Parameters.AddWithValue("@p1", TxtAd.Text);
+            komut.Parameters.AddWithValue("@p2", TxtYazar.Text);
+            komut.Parameters.AddWithValue("@p3", TxtSayfa.Text);
+            komut.Parameters.AddWithValue("@p4", TxtYayinEvi.Text);
+            komut.Parameters.AddWithValue("@p5", TxtTur.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Yeni Kitap Eklendi","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            listele();
+            sayac();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+
+            TxtId.Text = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            TxtAd.Text = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            TxtYazar.Text = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            TxtSayfa.Text = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            TxtYayinEvi.Text = dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+            TxtTur.Text = dataGridView1.Rows[secilen].Cells[5].Value.ToString();
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Delete from tblKITAPLAR where ID=@p1",baglanti);
+            komut.Parameters.AddWithValue("@p1", TxtId.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kitap Sistemden Silindi");
+            listele();
+            sayac();
+        }
+    }
+}
